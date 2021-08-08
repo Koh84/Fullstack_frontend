b1bb6ec4f83c2ed59a894b6bdd28e763357cbd9b
Kelvin <koh84@hotmail.com>
2021-08-08 23:48:58 +0100

    Chapter 5.49

--- src/App.js
+++ src/App.js
@@ -1,56 +1,77 @@
 import AdminWrapper from './components/AdminWrapper'
 import PageWrapper from './components/PageWrapper'
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { connect } from 'react-redux';
 
 // Pages
 import Home from './components/Pages/Home'
 import About from './components/Pages/About'
 import Contact from './components/Pages/Contact'
 import Login from './components/Pages/Login'
+import Dashboard from './components/Pages/Dashboard';
 
-function App() {
-  return (
-    <Router>
+class App extends Component {
+  render() {
+    return (
+      <Router>
 
-      <Route
-        path="/admin"
-        render={(props) => 
-          <AdminWrapper>
-            <Login/>
-          </AdminWrapper>}
-      />
+        <Route
+          path="/admin"
+          render={(props) => {
+            return(
+            <AdminWrapper>
+              {this.props.auth.token ? <Dashboard/> : <Login/>}
+            </AdminWrapper>)}
+            }
+        />
 
-      
-      <Route
-        exact path="/"
-        render={(props) => (
-          <PageWrapper>   
-            <Home {...props}/> 
-          </PageWrapper>
-        )}
-      />
-
-      <Route
-        path="/about"
-        render={(props) => (
-          <PageWrapper>   
-            <About {...props}/> 
-          </PageWrapper>
-        )}
-      />
-
-      <Route
-        path="/contact"
-        render={(props) => (
+        
+        <Route
+          exact path="/"
+          render={(props) => (
             <PageWrapper>   
-              <Contact {...props}/> 
+              <Home {...props}/> 
             </PageWrapper>
-        )}
-      />
-      
-    </Router>
-  );
+          )}
+        />
+
+        <Route
+          path="/about"
+          render={(props) => (
+            <PageWrapper>   
+              <About {...props}/> 
+            </PageWrapper>
+          )}
+        />
+
+        <Route
+          path="/contact"
+          render={(props) => (
+              <PageWrapper>   
+                <Contact {...props}/> 
+              </PageWrapper>
+          )}
+        />
+        
+      </Router>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
